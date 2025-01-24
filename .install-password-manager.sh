9b0a1f478cdc8e5a6d8e0a2b9c5abe9de23f6bec
DrummyFloyd <jonathan.monnet28@gmail.com>
2025-01-24 16:50:25 +0100

    fix(install-password-manager): change INSTALL_PATH based on distro

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -2,10 +2,10 @@
 
 # exit immediately if password-manager-binary is already in $PATH
 type rbw >/dev/null 2>&1 && exit
+INSTALL_PATH="$HOME/.local/bin"
 
 case "$(uname -s)" in
   Linux)
-    ARCH_NAME
     VERSION=$(curl -s https://api.github.com/repos/doy/rbw/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
     curl -Lo rbw.tar.gz "https://github.com/doy/rbw/releases/download/${VERSION}/rbw_${VERSION}_linux_amd64.tar.gz"
     # Extract the archive
@@ -13,8 +13,11 @@ case "$(uname -s)" in
     tar xf rbw.tar.gz --directory=/tmp/rbw-"${VERSION}"/
 
     # Install the binary
-    sudo mv /tmp/rbw-*/rbw /usr/bin/
-    sudo mv /tmp/rbw-*/rbw-agent /usr/bin/
+    if uname -a | grep -q "arch"; then
+      INSTALL_PATH="/usr/bin"
+    fi
+    sudo mv /tmp/rbw-*/rbw "${INSTALL_PATH}"/
+    sudo mv /tmp/rbw-*/rbw-agent "${INSTALL_PATH}"/
 
     # Cleanup
     rm -rf rbw.tar.gz /tmp/rbw/-*
