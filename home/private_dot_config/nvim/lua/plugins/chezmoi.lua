6a3edde91eb9f34951a73dca605f8ad195db412d
DrummyFloyd <jonathan.monnet28@gmail.com>
2025-05-22 18:25:48 +0200

    feat(nvim): add opts to chezmoi.nvim

---
+++ home/private_dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,14 @@
+return {
+  "xvzc/chezmoi.nvim",
+  opts = {
+    edit = {
+      watch = true,
+      force = true,
+    },
+    notification = {
+      on_open = false,
+      on_apply = true,
+      on_watch = true,
+    },
+  },
+}
