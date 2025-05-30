6860e93859243b6ec3415d3138eff148d3814b95
DrummyFloyd <jonathan.monnet28@gmail.com>
2025-05-30 21:42:02 +0200

    fix(nvim): laggy render neotree+edgy

--- home/private_dot_config/nvim/lua/plugins/neotree.lua
+++ home/private_dot_config/nvim/lua/plugins/neotree.lua
@@ -1,29 +1,37 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  dependencies = {
-    "s1n7ax/nvim-window-picker",
+  {
+    -- disable animation because laagy on neotree opening when default section are removed
+    "folke/edgy.nvim",
     opts = {
-      hint = "floating-big-letter",
-      show_prompt = false,
-      filter_rules = {
-        autoselect_one = true,
-        bo = {
-          filetype = { "NvimTree", "neo-tree", "notify", "smear-cursor" },
-
-          buftype = { "terminal", "quickfix", "nofile" },
+      animate = { enabled = false },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "s1n7ax/nvim-window-picker",
+      opts = {
+        hint = "floating-big-letter",
+        show_prompt = false,
+        filter_rules = {
+          autoselect_one = true,
+          bo = {
+            filetype = { "NvimTree", "neo-tree", "notify", "smear-cursor" },
+            buftype = { "terminal", "quickfix", "nofile" },
+          },
         },
       },
     },
-  },
-  opts = {
-    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline", "trouble", "edgy" },
-    sources = { "filesystem" },
-    window = {
-      mappings = {
-        ["S"] = "split_with_window_picker",
-        ["s"] = "vsplit_with_window_picker",
+    opts = {
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline", "trouble", "edgy" },
+      sources = { "filesystem" },
+      window = {
+        mappings = {
+          ["S"] = "split_with_window_picker",
+          ["s"] = "vsplit_with_window_picker",
+        },
       },
     },
+    keys = { { "<leader>be", false }, { "<leader>ge", false } },
   },
-  keys = { { "<leader>be", false }, { "<leader>ge", false } },
 }
