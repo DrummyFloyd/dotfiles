3ce1804662a18ecfae32f4fb21805a99737e0e3c
DrummyFloyd <jonathan.monnet28@gmail.com>
2025-12-03 22:19:16 +0100

    chore(nvim): completly disable codesnap for now

--- home/private_dot_config/nvim/lua/plugins/codesnap.lua
+++ home/private_dot_config/nvim/lua/plugins/codesnap.lua
@@ -1,54 +1,54 @@
 return {
-  {
-    "mistricky/codesnap.nvim",
-    event = "VeryLazy",
-    -- keys = function()
-    --   local wk = require("which-key")
-    --   wk.add({
-    --     {
-    --       mode = { "x" },
-    --       {
-    --         "<leader>cs",
-    --         "<Esc><cmd>CodeSnap<cr>",
-    --         icon = "󰄀",
-    --         desc = "CodeSnap to Clipboard",
-    --       },
-    --       {
-    --         "<leader>cS",
-    --         "<Esc><cmd>CodeSnapSave<cr>",
-    --         icon = "󰄀",
-    --         desc = "CodeSnap to File",
-    --       },
-    --       {
-    --         "<leader>ch",
-    --         "<Esc><cmd>CodeSnapHighlight<cr>",
-    --         icon = "󰄀",
-    --         desc = "CodeSnap Highlight to Clipboard",
-    --       },
-    --       {
-    --         "<leader>cH",
-    --         "<Esc><cmd>CodeSnapSaveHighlight<cr>",
-    --         icon = "󰄀",
-    --         desc = "CodeSnap Highlight to File",
-    --       },
-    --       {
-    --         "<leader>cA",
-    --         "<Esc><cmd>CodeSnapASCII<cr>",
-    --         icon = "󰄀",
-    --         desc = "CodeSnap ASCII to Clipboard",
-    --       },
-    --     },
-    --   })
-    -- end,
-    -- opts = {
-    --   mac_window_bar = false,
-    --   save_path = "~/Pictures/",
-    --   has_breadcrumbs = true,
-    --   show_workspace = true,
-    --   bg_theme = "sea",
-    --   watermark = "",
-    --   code_font_family = "FiraCode Nerd Font",
-    --   has_line_number = true,
-    -- },
-  },
+  -- {
+  --   "mistricky/codesnap.nvim",
+  --   event = "VeryLazy",
+  --   -- keys = function()
+  --   --   local wk = require("which-key")
+  --   --   wk.add({
+  --   --     {
+  --   --       mode = { "x" },
+  --   --       {
+  --   --         "<leader>cs",
+  --   --         "<Esc><cmd>CodeSnap<cr>",
+  --   --         icon = "󰄀",
+  --   --         desc = "CodeSnap to Clipboard",
+  --   --       },
+  --   --       {
+  --   --         "<leader>cS",
+  --   --         "<Esc><cmd>CodeSnapSave<cr>",
+  --   --         icon = "󰄀",
+  --   --         desc = "CodeSnap to File",
+  --   --       },
+  --   --       {
+  --   --         "<leader>ch",
+  --   --         "<Esc><cmd>CodeSnapHighlight<cr>",
+  --   --         icon = "󰄀",
+  --   --         desc = "CodeSnap Highlight to Clipboard",
+  --   --       },
+  --   --       {
+  --   --         "<leader>cH",
+  --   --         "<Esc><cmd>CodeSnapSaveHighlight<cr>",
+  --   --         icon = "󰄀",
+  --   --         desc = "CodeSnap Highlight to File",
+  --   --       },
+  --   --       {
+  --   --         "<leader>cA",
+  --   --         "<Esc><cmd>CodeSnapASCII<cr>",
+  --   --         icon = "󰄀",
+  --   --         desc = "CodeSnap ASCII to Clipboard",
+  --   --       },
+  --   --     },
+  --   --   })
+  --   -- end,
+  --   -- opts = {
+  --   --   mac_window_bar = false,
+  --   --   save_path = "~/Pictures/",
+  --   --   has_breadcrumbs = true,
+  --   --   show_workspace = true,
+  --   --   bg_theme = "sea",
+  --   --   watermark = "",
+  --   --   code_font_family = "FiraCode Nerd Font",
+  --   --   has_line_number = true,
+  --   -- },
+  -- },
 }
